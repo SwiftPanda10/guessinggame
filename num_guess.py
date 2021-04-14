2d8b14c7e0c9ed21d6cb0151838ab4a219811dc1
Sam Bennett <samuelhb201@gmail.com>
2021-04-14 10:29:42 -0700

    A guessing game where user one enters an integer and player two attempts to guess it using looped prompts

---
+++ num_guess.py
@@ -0,0 +1,21 @@
+# Author: Samuel Bennett
+# Date: 4/14/2021
+# Description: A guessing game where user one enters an integer and player two attempts to guess it using looped prompts
+
+tries = 1
+print("Enter the integer for the player to guess.")
+answer = int(input())
+print("Enter your guess.")
+guess = int(input())
+
+while guess != answer:
+    if guess < answer:
+        print("Too low - try again:")
+        guess = int(input())
+        tries = tries + 1
+    elif guess > answer:
+        print("Too high - try again:")
+        guess = int(input())
+        tries = tries + 1
+
+print("You guessed it in", tries, "tries")
